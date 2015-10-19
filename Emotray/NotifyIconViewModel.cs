06cede963ea63727ffa55fdf07a8bdb8da1e4a0d
geoffles <geoffles@gmail.com>
2015-10-19 09:33:22 +0200

    Initial commit with the app.

---
+++ Emotray/NotifyIconViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Emotray
+{
+    /// <summary>
+    /// Provides bindable properties and commands for the NotifyIcon. In this sample, the
+    /// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
+    /// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
+    /// </summary>
+    public class NotifyIconViewModel
+    {
+        public ICommand ShowAbout
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CanExecuteFunc = () => Application.Current.MainWindow == null,
+                    CommandAction = () =>
+                    {
+                        Application.Current.MainWindow = new MainWindow();
+                        Application.Current.MainWindow.Show();
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Shows a window, if none is already open.
+        /// </summary>
+        public ICommand CopyToClipboardCommand
+        {
+            get
+            {
+                return new EmoteCommand();
+            }
+        }
+
+
+        /// <summary>
+        /// Shuts down the application.
+        /// </summary>
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand {CommandAction = () => Application.Current.Shutdown()};
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Simplistic delegate command for the demo.
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null  || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    /// <summary>
+    /// Simplistic delegate command for the demo.
+    /// </summary>
+    public class EmoteCommand : ICommand
+    {
+        //public Action<string> CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            Clipboard.SetText((string)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
