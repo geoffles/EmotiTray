ee59a09d10ae70e0efbc731e434438b94b6b8a7f
geoffles <geoffles@gmail.com>
2015-10-19 12:50:23 +0200

    Add readme. Some renaming.

--- EmotiTray/Properties/AssemblyInfo.cs
+++ EmotiTray/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Emotray")]
+[assembly: AssemblyTitle("EmotiTray")]
 [assembly: AssemblyDescription("Emote text tray app")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Emotray")]
+[assembly: AssemblyProduct("EmotiTray")]
 [assembly: AssemblyCopyright("Copyright © G.Lydall 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ README.md
@@ -0,0 +1,13 @@
+# EmotiTray
+
+Emote stuff in your conversations!
+
+Simply rightclick on the tray app to copy the text emoticon.
+
+![See the clickiness](https://cloud.githubusercontent.com/assets/870474/10575828/7b635974-765f-11e5-9b6e-adede16fc6c7.png)
+
+## Installation instructions
+
+1.  Unpack to folder (Eg: C:\Program files (x86)\EmotiTray)
+2.  Pin EmotiTray.exe to your start menu (right click the file)
+3.  (Optional) Run "shell:startup" and copy a shortcut there to startup automagically
